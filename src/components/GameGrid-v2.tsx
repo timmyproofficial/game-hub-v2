efc45f75ac11723262bd98b93b95fb7af7780657
timmyproofficial <timmyshow96@gmail.com>
2024-07-16 15:38:04 +0100

    Fix build error and install react router dom.

--- src/components/GameGrid-v2.tsx
+++ src/components/GameGrid-v2.tsx
@@ -3,7 +3,7 @@ import useGames from '../hooks/useGames-v2';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
-import { GameQuery } from '../App';
+import { GameQuery } from '../store';
 
 interface Props {
   gameQuery: GameQuery;
