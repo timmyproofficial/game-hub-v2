6eade74bce8bc9a2e598e25c84b366b4ed2401c5
timmyproofficial <timmyshow96@gmail.com>
2024-07-17 12:08:54 +0100

    Fix build error.

--- src/components/GameGrid-v2.tsx
+++ src/components/GameGrid-v2.tsx
@@ -1,43 +1,45 @@
-import { SimpleGrid, Text } from '@chakra-ui/react';
-import useGames from '../hooks/useGames-v2';
-import GameCard from './GameCard';
-import GameCardSkeleton from './GameCardSkeleton';
-import GameCardContainer from './GameCardContainer';
-import { GameQuery } from '../store';
+// import { SimpleGrid, Text } from '@chakra-ui/react';
+// import useGames from '../hooks/useGames-v2';
+// import GameCard from './GameCard';
+// import GameCardSkeleton from './GameCardSkeleton';
+// import GameCardContainer from './GameCardContainer';
+// import { GameQuery } from '../store';
 
-interface Props {
-  gameQuery: GameQuery;
-}
+// interface Props {
+//   gameQuery: GameQuery;
+// }
 
-const GameGrid = ({ gameQuery }: Props) => {
-  const { data, error, isLoading } = useGames(gameQuery);
-  const skeletons = [1, 2, 3, 4, 5, 6];
+// const GameGrid = ({ gameQuery }: Props) => {
+//   const { data, error, isLoading } = useGames(gameQuery);
+//   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <Text>{error.message}</Text>;
+//   if (error) return <Text>{error.message}</Text>;
 
-  return (
-    <>
-      {/* {error && <Text>{error}</Text>} */}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={6}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              {' '}
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data?.results.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
-          </GameCardContainer>
-        ))}
-      </SimpleGrid>
-    </>
-  );
-};
+//   return (
+//     <>
+//       {/* {error && <Text>{error}</Text>} */}
+//       <SimpleGrid
+//         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+//         padding="10px"
+//         spacing={6}
+//       >
+//         {isLoading &&
+//           skeletons.map((skeleton) => (
+//             <GameCardContainer key={skeleton}>
+//               {' '}
+//               <GameCardSkeleton />
+//             </GameCardContainer>
+//           ))}
+//         {data?.results.map((game) => (
+//           <GameCardContainer key={game.id}>
+//             <GameCard game={game} />
+//           </GameCardContainer>
+//         ))}
+//       </SimpleGrid>
+//     </>
+//   );
+// };
 
-export default GameGrid;
+// export default GameGrid;
+
+export {};
